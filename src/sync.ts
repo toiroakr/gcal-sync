0f10cf5c45d1c87c6f9878ce9cbf5b96240b9d81
Akira HIGUCHI <akira_higuchi@tailor.tech>
2025-05-27 22:28:28 +0900

    fix bug

--- src/sync.ts
+++ src/sync.ts
@@ -4,21 +4,17 @@ type Config = ReturnType<typeof getConfig>;
 function onCalendarEdit() {
   const config = getConfig();
 
-  const scriptProperties = PropertiesService.getScriptProperties();
-  const properties = scriptProperties.getProperties();
-
-  let events = getEvents(config.sourceCalendarId, { syncToken: properties.syncToken });
-  while (properties.syncToken && events && events.items) {
+  let events = getEvents(config.sourceCalendarId);
+  while (events && events.items) {
     syncEvent(events.items, config);
     if (events.nextPageToken) {
-      events = getEvents(config.sourceCalendarId, { pageToken: events.nextPageToken });
+      events = getEvents(config.sourceCalendarId, {
+        pageToken: events.nextPageToken,
+      });
     } else {
       break;
     }
   }
-
-  scriptProperties.setProperty("syncToken", events?.nextSyncToken ?? "");
-  console.log(scriptProperties.getProperties());
 }
 
 function processAll() {
@@ -27,12 +23,16 @@ function processAll() {
   const scriptProperties = PropertiesService.getScriptProperties();
   const properties = scriptProperties.getProperties();
 
-  let events = getEvents(config.sourceCalendarId, { pageToken: properties.pageToken ?? undefined });
-  while (properties.syncToken && events && events.items) {
+  let events = getEvents(config.sourceCalendarId, {
+    pageToken: properties.pageToken ?? undefined,
+  });
+  while (events && events.items) {
     syncEvent(events.items, config);
     if (events.nextPageToken) {
       scriptProperties.setProperty("pageToken", events?.nextPageToken ?? "");
-      events = getEvents(config.sourceCalendarId, { pageToken: events.nextPageToken });
+      events = getEvents(config.sourceCalendarId, {
+        pageToken: events.nextPageToken,
+      });
     } else {
       break;
     }
@@ -40,10 +40,42 @@ function processAll() {
   scriptProperties.deleteProperty("pageToken");
 }
 
-function getEvents(calendarId: string, option: { syncToken?: string, pageToken?: string } = {}) {
+function reverseCheck() {
+  const config = getConfig();
+
+  const scriptProperties = PropertiesService.getScriptProperties();
+  const properties = scriptProperties.getProperties();
+
+  let events = getEvents(config.targetCalendarId, {
+    pageToken: properties.reversePageToken ?? undefined,
+  });
+  while (events && events.items) {
+    reverseSyncEvent(events.items, config);
+    if (events.nextPageToken) {
+      scriptProperties.setProperty(
+        "reversePageToken",
+        events?.nextPageToken ?? "",
+      );
+      events = getEvents(config.targetCalendarId, {
+        pageToken: events.nextPageToken,
+      });
+    } else {
+      break;
+    }
+  }
+  scriptProperties.deleteProperty("reversePageToken");
+}
+
+function getEvents(
+  calendarId: string,
+  option: { pageToken?: string } = {},
+) {
   try {
     const today = new Date();
     today.setHours(0, 0, 0, 0);
+    const twoWeeksAfter = new Date();
+    twoWeeksAfter.setHours(0, 0, 0, 0);
+    twoWeeksAfter.setDate(twoWeeksAfter.getDate() + 7);
 
     return Calendar.Events?.list(
       calendarId,
@@ -51,6 +83,7 @@ function getEvents(calendarId: string, option: { syncToken?: string, pageToken?:
         ...option,
         maxResults: 9999,
         timeMin: today.toISOString(),
+        timeMax: twoWeeksAfter.toISOString(),
       },
     );
   } catch (e) {
@@ -61,39 +94,76 @@ function getEvents(calendarId: string, option: { syncToken?: string, pageToken?:
   }
 }
 
-function syncEvent(events: GoogleAppsScript.Calendar.Schema.Event[], config: Config) {
-  events.forEach((event) => {
-    const newEvent = {
-      ...event,
-      colorId: config.targetColorId,
-    };
+function syncEvent(
+  events: GoogleAppsScript.Calendar.Schema.Event[],
+  config: Config,
+) {
+  events
+    .forEach((event) => {
+      const newEvent = {
+        ...event,
+        colorId: config.targetColorId,
+      };
 
-    console.log(event);
-    let existingEvent: CalendarEvent | undefined;
-    if (event.id) {
-      try {
-        existingEvent = Calendar.Events?.get(config.targetCalendarId, event.id);
-        Calendar.Events?.update(newEvent, config.targetCalendarId, event.id, { sendUpdates: 'none' }, { 'If-Match': existingEvent?.etag });
-        console.log("update existing event");
-        return;
-      } catch (e) {
-        console.log(e);
-      }
-      try {
-        if (event.status === 'cancelled') {
-          if (existingEvent) {
-            Calendar.Events?.remove(config.targetCalendarId, event.id!, { sendUpdates: 'none' });
-            console.log("removed existing event");
-          }``
-        } else {
-          Calendar.Events?.insert(newEvent, config.targetCalendarId, { sendUpdates: 'none' });
-          console.log("inserted new event");
+      console.log(event);
+      let existingEvent: CalendarEvent | undefined;
+      if (event.id) {
+        try {
+          existingEvent = Calendar.Events?.get(config.targetCalendarId, event.id);
+          Calendar.Events?.update(newEvent, config.targetCalendarId, event.id, {
+            sendUpdates: "none",
+          }, { "If-Match": existingEvent?.etag });
+          console.log("update existing event");
+          return;
+        } catch (e) {
+          console.log(e);
         }
-      } catch (e) {
-        console.log(e);
+        try {
+          if (event.status === "cancelled") {
+            if (existingEvent) {
+              Calendar.Events?.remove(config.targetCalendarId, event.id!, {
+                sendUpdates: "none",
+              });
+              console.log("removed existing event");
+            }
+          } else {
+            Calendar.Events?.insert(newEvent, config.targetCalendarId, {
+              sendUpdates: "none",
+            });
+            console.log("inserted new event");
+          }
+        } catch (e) {
+          console.log(e);
+        }
       }
-    }
-  })
+    });
+}
+
+function reverseSyncEvent(
+  events: GoogleAppsScript.Calendar.Schema.Event[],
+  config: Config,
+) {
+  events
+    .filter((event) => event.colorId === config.targetColorId)
+    .forEach((event) => {
+      let sourceEvent: CalendarEvent | undefined;
+      if (event.id) {
+        try {
+          sourceEvent = Calendar.Events?.get(config.sourceCalendarId, event.id);
+
+          if (sourceEvent) {
+            console.log("update existing event");
+            Calendar.Events?.update(sourceEvent, config.targetCalendarId, event.id, { sendUpdates: 'none' }, { 'If-Match': event?.etag });
+          }
+          return;
+        } catch (e) {
+          console.log(e);
+        }
+      } else {
+        // If the event does not have an ID, we cannot update or remove it.
+        console.log("Event does not have an ID, skipping.");
+      }
+    });
 }
 
 function getConfig() {
